f61c78d7865c466547bf91dc5e8c883fd481d723
cleber-teixeira <cleberlt@gmail.com>
2022-02-09 22:56:08 -0300

    Atualiza a página do perfil e o "style" da página projetos. :white_check_mark:

--- src/components/PersonalInfo.js
+++ src/components/PersonalInfo.js
@@ -8,27 +8,70 @@ import './PersonalInfo.css';
 class PersonalInfo extends React.Component {
   render() {
     return (
-      <aside id="personal-info">
-        <img src={ profileImg } alt="Foto Cleber Teixeira" className="img-profile" />
+		<aside id='personal-info'>
+			<img
+				src={profileImg}
+				alt='Foto Cleber Teixeira'
+				className='img-profile'
+			/>
 
-        <h2>Cleber Lopes Teixeira</h2>
+			<h2>Cleber Lopes Teixeira</h2>
 
-        <p>Moro em: <span>São Paulo/SP</span> | Natural de: <span>Fortaleza-CE</span></p>
+			<p>
+				Moro em: <span>São Paulo/SP</span> | Natural de:{' '}
+				<span>Fortaleza-CE</span>
+			</p>
 
-        <p>
-          <a href="https://github.com/cleber-teixeira" target="_blank" rel="noopener noreferrer">
-            <img src={ githubIcon } alt="Ícone do GitHub" className="icon" />
-          </a>
-          <a href="https://www.linkedin.com/in/cleberlopesteixeira/" target="_blank" rel="noopener noreferrer">
-            <img src={ linkedinIcon } alt="Ícone do Linkedin" className="icon" />
-          </a>
-        </p>
+			<p>
+				<a
+					href='https://github.com/cleber-teixeira'
+					target='_blank'
+					rel='noopener noreferrer'
+				>
+					<img
+						src={githubIcon}
+						alt='Ícone do GitHub'
+						className='icon'
+					/>
+				</a>
+				<a
+					href='https://www.linkedin.com/in/cleberlopesteixeira/'
+					target='_blank'
+					rel='noopener noreferrer'
+				>
+					<img
+						src={linkedinIcon}
+						alt='Ícone do Linkedin'
+						className='icon'
+					/>
+				</a>
+			</p>
+			<a
+				href='../images/curriculo_cleber-teixeira.zip'
+				Content-Type='.application/zip'
+				download
+				className='curriculo'
+			>
+				Curriculo
+			</a>
 
-        <p className="sobre">
-          Sou apaixonado por <span><em>tecnologia</em></span>, música, natureza e <span>esportes</span> (principalmente <span><em>artes-marciais</em></span>). Atualmente sou aluno do curso de <span>Desenvolvimento Web Full Stack da <a href="https://www.betrybe.com/" target="_blank" >Trybe</a> - <em>Turma 09</em>.</span> Estou me preparando para ser um profissional exemplar e <em>fazer a diferença na vida das pessoas!</em>
-        </p>
-      </aside>
-    );
+			<p className='sobre'>
+				Sou apaixonado por tecnologia, música, natureza e esportes
+				(principalmente artes-marciais). Me formei como{' '}
+				<span>
+					Desenvolvedor Web Full Stack pela{' '}
+					<a href='https://www.betrybe.com/' target='_blank'>
+						Trybe
+					</a>{' '}
+					- <em>Turma 09</em>.
+				</span>{' '}
+				Atualmente estou aberto à oportunidades como desenvolvedor
+				júnior, onde pretendo continuar evoluindo e me preparando para
+				ser um profissional exemplar e{' '}
+				<em>fazer a diferença na vida das pessoas!</em>
+			</p>
+		</aside>
+	);
   }
 }
 
